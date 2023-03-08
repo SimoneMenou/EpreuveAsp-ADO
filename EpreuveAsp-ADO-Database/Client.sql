298328a8316c628f9f933810e7a94482175146a4
Menen <szymonmenen1@gmail.com>
2023-03-08 16:39:43 +0100

    Add project files.

---
+++ EpreuveAsp-ADO-Database/Client.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Client]
+(
+	[IdClient] INT NOT NULL PRIMARY KEY, 
+    [Nom] NVARCHAR(50) NULL, 
+    [Prenom] NVARCHAR(50) NULL, 
+    [AdresseMail] NCHAR(255) NULL,
+    [MotDePasse] NVARCHAR(MAX) NULL, 
+    [Pays] NVARCHAR(50) NULL, 
+    [Telephone] NCHAR(10) NULL, 
+   
+    
+)
